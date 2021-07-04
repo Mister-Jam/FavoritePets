105d2edb2db64fd361ff5bda2131f48d03806432
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-07-04 01:09:12 +0100

    customized tab bar and transition

--- FavoritePets/Controllers/HomeTabBarController.swift
+++ FavoritePets/Controllers/HomeTabBarController.swift
@@ -11,19 +11,40 @@ class HomeTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let allCatsTabBar = AllCatsViewController()
+        let catsILikeTabBar = CatsILikeViewController(collectionViewLayout: CatsILikeViewController.collectionViewLayout())
+        delegate = self
+        
+        allCatsTabBar.title = "All Cats"
+        catsILikeTabBar.title = "Cats I Like"
+        
+        let allCatsHome = UINavigationController(rootViewController: allCatsTabBar)
+        let catsILikeHome = UINavigationController(rootViewController: catsILikeTabBar)
+        
+        allCatsHome.tabBarItem = UITabBarItem(title: "All cats", image: UIImage(named: "CatTabVector"), tag: 0)
+        allCatsHome.tabBarItem.setTitleTextAttributes(Constants.tabBarText, for: .normal)
+        catsILikeHome.tabBarItem = UITabBarItem(title: "Cats I like", image: UIImage(named: "LikeVector"), tag: 1)
+        catsILikeHome.tabBarItem.setTitleTextAttributes(Constants.tabBarText, for: .normal)
+        
+       
+        tabBar.barTintColor = .white
+        tabBar.tintColor = .black
+        
+        allCatsHome.navigationBar.prefersLargeTitles = true
+        catsILikeHome.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers([allCatsHome, catsILikeHome], animated: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension HomeTabBarController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return CustomTransition(viewControllers: tabBarController.viewControllers)
+    }
+}
+
+
