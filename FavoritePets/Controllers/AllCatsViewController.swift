97221b53d58a444ab3b20ace0a44eb47cbe664d7
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-07-04 01:07:12 +0100

    design general pets screen

--- FavoritePets/Controllers/AllCatsViewController.swift
+++ FavoritePets/Controllers/AllCatsViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class AllCatsViewController: UIViewController {
+class AllCatsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        tableView.register(AllCatsTableViewCell.self, forCellReuseIdentifier: String(describing: AllCatsTableViewCell.self))
+        navigationController?.navigationBar.largeTitleTextAttributes = Constants.titleText
+        tableView.separatorStyle = .none
+        
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: AllCatsTableViewCell.self), for: indexPath) as! AllCatsTableViewCell
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 
 
 }
