42a5d89787e622e41d594ee37e393e760df73cda
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-07-06 13:30:27 +0100

    refactor url session

--- FavoritePets/Resources/NetworkManager.swift
+++ FavoritePets/Resources/NetworkManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+final class NetworkManager {
+    
+    static let shared   = NetworkManager()
+    
+    private let path    = "v1/breeds"
+    //MARK: Pass in URL and get a decoded Data
+    func getCatsData(completion: @escaping ((Result<[CatsDataModel], Error>)->Void)) {
+        guard let urlrequest = URL(string: Constants.UrlConstants.baseUrl+path) else { return }
+        URLSession.shared.decodeData(from: urlrequest, type: [CatsDataModel].self) { result in
+            switch result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+
+    }
+}
